2790f3ad9a1848fe24579f1053acf61a2c4d6371
Wowaxur <wowaxur@yandex.ru>
2024-12-07 11:44:45 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import {BrowserRouter, Routes,Route} from "react-router-dom";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import OnlineMagazine from "./components/Pages/onlineMagazine/OnlineMagazine";
 import Layout from "./components/Pages/layout/layour";
 import NoPage from "./components/Pages/noPage/noPage";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<Layout />}/>
+                <Route path="/" element={<Layout />} />
                 <Route path="Magazine" element={<OnlineMagazine />} />
                 <Route path="*" element={<NoPage />} />
             </Routes>
-        </BrowserRouter>
-
+        </HashRouter>
     );
 }
-
-
-//router 
