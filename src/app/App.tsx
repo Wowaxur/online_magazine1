6a0421369330aac366d67fa05d6f80a2fc217b06
Wowaxur <wowaxur@yandex.ru>
2024-12-24 00:08:48 +0300

    add CategoryPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,17 @@ import OnlineMagazine from "~/Pages/onlineMagazine/OnlineMagazine";
 import NoPage from "~/Pages/noPage/noPage";
 import {Provider} from "react-redux";
 import {store} from "~/app/store/store";
+import CategoryPage from "~/Features/mainContent/CategoryPage/CategoryPage";
 
 export default function App() {
+
+
     return (
         <Provider store={store}>
         <HashRouter>
             <Routes>
-                <Route path="/" element={<OnlineMagazine />} />
+                <Route path="/*" element={<OnlineMagazine />} />
+                <Route path="/category/:categoryName" element={<CategoryPage  />} />
                 <Route path="/*" element={<NoPage />} />
             </Routes>
         </HashRouter>
