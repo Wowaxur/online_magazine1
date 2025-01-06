d0e2b50fe981b85747afc7a4d79f6a53a519c58b
Wowaxur <wowaxur@yandex.ru>
2025-01-06 19:08:59 +0300

    add auth0 provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "~/app/App";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const domain = "dev-ibbyn3ra8476rmqf.us.auth0.com";
+const clientId = "kwBJzkF79v7bhfMDrWroYYiPgFKYXHX6";
 root.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+          domain={domain}
+          clientId={clientId}
+          authorizationParams={{
+              redirect_uri: window.location.origin
+          }}
+      >
+          <App />
+      </Auth0Provider>
   </React.StrictMode>
 );
 
