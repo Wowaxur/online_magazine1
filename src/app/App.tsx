b73066ac220dcfc4cab3470f164378d58a1a571a
Wowaxur <wowaxur@yandex.ru>
2025-01-06 19:07:57 +0300

    add ProtectedRoute

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,39 @@
 import React from "react";
-import {HashRouter, Routes, Route} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import OnlineMagazine from "~/Pages/onlineMagazine/OnlineMagazine";
 import NoPage from "~/Pages/noPage/noPage";
-import {Provider} from "react-redux";
-import {store} from "~/app/store/store";
 import CategoryPage from "~/Features/mainContent/CategoryPage/CategoryPage";
+import { Provider } from "react-redux";
+import { store } from "~/app/store/store";
+import ProtectedRoute from "~/Features/ProtectedRoute/ProtectedRoute";
+import LoginPage from "~/Pages/LoginPage/LoginPage";
+
 
 export default function App() {
-
-
     return (
-        <Provider store={store}>
-        <HashRouter>
-            <Routes>
-                <Route path="/*" element={<OnlineMagazine />} />
-                <Route path="/category/:categoryName" element={<CategoryPage  />} />
-                <Route path="/*" element={<NoPage />} />
-            </Routes>
-        </HashRouter>
-        </Provider>
+            <Provider store={store}>
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
+                        <Route
+                            path="/News"
+                            element={
+                                <ProtectedRoute>
+                                    <OnlineMagazine />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path="/category/:categoryName"
+                            element={
+                                <ProtectedRoute>
+                                    <CategoryPage />
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route path="/*" element={<NoPage />} />
+                    </Routes>
+                </HashRouter>
+            </Provider>
     );
 }
