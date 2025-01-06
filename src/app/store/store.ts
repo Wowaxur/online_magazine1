1c5f381393a2a88ccefa7f933daa5b3327be926a
Wowaxur <wowaxur@yandex.ru>
2025-01-06 19:10:03 +0300

    add useAppDispatch

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import newsReducer from '~/shared/api/newsSlice';
 import { okSurfApiSlice } from '~/shared/api/apiSlice';
+import {useDispatch} from "react-redux";
 
 export const store = configureStore({
     reducer: {
@@ -13,3 +14,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
